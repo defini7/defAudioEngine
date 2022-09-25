e555336114eea7622de4ec9b73f538b32d6ca33a
Алекс <75216201+defini7@users.noreply.github.com>
2022-09-25 15:38:53 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,9 +2,32 @@
 
 #include "defAudioEngine.h"
 
+class Example : public def::AudioEngine
+{
+public:
+	Example()
+	{
+
+	}
+
+private:
+	// Overridden by user if they want to generate sound in real-time
+	virtual float OnUserSoundSample(int nChannel, float fGlobalTime, float fTimeStep)
+	{
+		return 0.0f;
+	}
+
+	// Overriden by user if they want to manipulate the sound before it is played
+	virtual float OnUserSoundFilter(int nChannel, float fGlobalTime, float fSample)
+	{
+		return sinf(fSample) * fGlobalTime;
+	}
+
+};
+
 int main()
 {
-	Audio audio;
+	Example audio;
 
 	int nSample1 = audio.LoadAudioSample("p1elim.wav");
 
