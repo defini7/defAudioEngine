31e0d774ef9a4e8dbe65cbd776b80c2c223eca68
Алекс <75216201+defini7@users.noreply.github.com>
2022-10-01 22:48:11 +0300

    Update defAudioEngine.h

--- defAudioEngine.h
+++ defAudioEngine.h
@@ -1,5 +1,62 @@
 #pragma once
 
+#pragma region license
+/***
+*	BSD 3-Clause License
+	Copyright (c) 2021, 2022 Alex
+	All rights reserved.
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+	1. Redistributions of source code must retain the above copyright notice, this
+	   list of conditions and the following disclaimer.
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+	3. Neither the name of the copyright holder nor the names of its
+	   contributors may be used to endorse or promote products derived from
+	   this software without specific prior written permission.
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***/
+#pragma endregion
+
+#pragma region sample
+/**
+	#include <iostream>
+
+	#include "defAudioEngine.h"
+
+	int main()
+	{
+		// Creating instance of main class
+		def::AudioEngine audio;
+
+		// Loading audio in queue and getting it's id
+		int nSample1 = audio.LoadAudioSample("p1elim.wav");
+
+		// Playing sample
+		audio.PlaySample(nSample1, true);
+
+		// Because audio engine runs on different thread 
+		// we need to check if there are any active (currently playing) samples
+		while (audio.listActiveSamples.size() > 0) {}
+
+		// "Tell" to the engine that we want to stop it
+		audio.DestroyAudio();
+
+		return 0;
+	}
+**/
+#pragma endregion
+
 #if !defined(DEF_PLATFORM_CUSTOM)
 	#if defined(_WIN32)
 		#define DEF_PLATFORM_WINMM
@@ -518,3 +575,4 @@ namespace def
 #endif
 	};
 }
+
